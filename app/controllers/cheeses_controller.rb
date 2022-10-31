b9be98053a34c7dd845461c8549338da74fd5eee
Zachary Youngdale <zyoungdale@gmail.com>
2022-10-31 15:58:54 -0400

    commit tree 7daf9026b2102ac0b1089d29bfaa502f773583b9

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -3,13 +3,13 @@ class CheesesController < ApplicationController
   # GET /cheeses
   def index
     cheeses = Cheese.all
-    render json: cheeses
+    render json: cheeses, only: [:id, :name, :price, :is_best_seller], methods: [:summary]
   end
 
   # GET /cheeses/:id
   def show
     cheese = Cheese.find_by(id: params[:id])
-    render json: cheese
+    render json: cheese, except: [:created_at, :updated_at], methods: [:summary]
   end
 
 end
